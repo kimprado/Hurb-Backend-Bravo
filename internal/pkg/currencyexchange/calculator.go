550f0e45d3597551cc8902cce93cf18f80fa730d
Kim Prado <kimprado21@gmail.com>
2019-12-08 11:44:45 -0300

    Criar currencyexchange CalculatorController
    Declarar interfaces do controlador do domínio.

---
+++ internal/pkg/currencyexchange/calculator.go
@@ -0,0 +1,24 @@
+package currencyexchange
+
+// Calculator é um ponto de entrada para comportamento
+// da aplicação. O controlador do domínio.
+type Calculator interface {
+	Calculate()
+}
+
+// CurrencyAdder é um ponto de entrada para comportamento
+// da aplicação. O controlador do domínio.
+type CurrencyAdder interface {
+	Add()
+}
+
+// CurrencyRemover é um ponto de entrada para comportamento
+// da aplicação. O controlador do domínio.
+type CurrencyRemover interface {
+	Remove()
+}
+
+// CalculatorController é o ponto de entrada para comportamentos
+// da aplicação. Implementação do controlador do domínio.
+type CalculatorController struct {
+}
