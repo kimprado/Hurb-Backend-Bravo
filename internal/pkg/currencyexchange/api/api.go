8ec272702e5a14431aaa0de3725fb86b419f05f1
Kim Prado <kimprado21@gmail.com>
2019-12-10 06:05:19 -0300

    Validar parâmetros e retornar problemas

--- internal/pkg/currencyexchange/api/api.go
+++ internal/pkg/currencyexchange/api/api.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/rep/exchange/internal/pkg/commom/errors"
 	"github.com/rep/exchange/internal/pkg/commom/logging"
 	"github.com/rep/exchange/internal/pkg/commom/web"
 	"github.com/rep/exchange/internal/pkg/currencyexchange"
@@ -27,24 +28,63 @@ func NewController(c currencyexchange.Calculator, l logging.LoggerAPIExchange) (
 
 // Exchange calcula câmbio
 func (v *Controller) Exchange(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	var err error
 
 	queryValues := req.URL.Query()
+
+	paramErr := errors.NewParametersError()
 
 	from := queryValues.Get("from")
 	to := queryValues.Get("to")
 	strAmount := queryValues.Get("amount")
 
-	amount, err := strconv.ParseFloat(strAmount, 64)
+	if from == "" {
+		paramErr.Add(
+			errors.ParameterError{
+				Name:   "from",
+				Value:  from,
+				Reason: "'from' não pode ser vazio",
+			},
+		)
+	}
+	if to == "" {
+		paramErr.Add(
+			errors.ParameterError{
+				Name:   "to",
+				Value:  to,
+				Reason: "'to' não pode ser vazio",
+			},
+		)
+	}
+	if strAmount == "" {
+		paramErr.Add(
+			errors.ParameterError{
+				Name:   "amount",
+				Value:  strAmount,
+				Reason: "'amount' não pode ser vazio",
+			},
+		)
+	}
 
-	if err != nil {
-		v.logger.Errorf("Consulta Exchange %+v\n", params)
-		res.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	amount, err := strconv.ParseFloat(strAmount, 64)
+	if strAmount != "" && err != nil {
+		paramErr.Add(
+			errors.ParameterError{
+				Name:   "amount",
+				Value:  strAmount,
+				Reason: fmt.Sprintf("Não foi possivel converter %q. Informe valor monetário. Ex: 123.45", strAmount),
+			},
+		)
+	}
+
+	if paramErr.ContainsError() {
+		v.logger.Warnf("Consulta Exchange com parâmetro(s) inválido(s):\n\t%v\n", queryValues)
 
 		web.NewHTTPResponse(
 			res,
 			http.StatusBadRequest,
 			nil,
-			web.NewErrorMessage(fmt.Sprintf("Amount %q is invalid", strAmount)),
+			paramErr,
 		).WriteJSON()
 
 		return
