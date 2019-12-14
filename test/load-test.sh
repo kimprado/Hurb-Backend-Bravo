d76683fc1e3528d78ee7d123f67ffd8b5d2a61c3
Kim Prado <kimprado21@gmail.com>
2019-12-14 04:30:55 -0300

    Ajustar teste de carga

--- test/load-test.sh
+++ test/load-test.sh
@@ -3,7 +3,7 @@
 set -e
 clear
 
-ab -n 60000 -c 10 -t 60 -m GET http://localhost:80/api/exchange?from=USD\&to=EUR\&amount=5
+ab -n 60000 -c 10 -m GET http://localhost:80/api/exchange?from=USD\&to=EUR\&amount=5
 
 # Verificar status do encerramento do Teste ab, com "$?"
 if [ $? -eq 0 ]
